5c9878865503445eb3a8f9a975b64b9c37e54298
DevSanyamSrivastava <sanyam.fullstackdev@gmail.com>
2026-07-14 11:52:46 +0530

    add js files

---
+++ 04_js/04_naming_convension.js
@@ -0,0 +1,15 @@
+// let @a = "18"
+
+// let 1a ="hello"
+// let a1 = 17
+
+// let class = "hello";
+
+// let use@name=56
+
+
+let userName = 15
+let UserName= 89
+let username=98
+
+console.log(userName, UserName, username )
