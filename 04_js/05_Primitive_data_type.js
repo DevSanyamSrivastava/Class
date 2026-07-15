75f7693f047045a57ba98a90b5dbb4a6207649ef
Sanyamm Srivastava <163429562+DevSanyamSrivastava@users.noreply.github.com>
2026-07-15 11:50:49 +0530

    Js data type

---
+++ 04_js/05_Primitive_data_type.js
@@ -0,0 +1,20 @@
+//Primitive Data type  Number BigInt String Boolean Symbol Undefined Null
+//Number
+let age = 12;
+let mark = 13
+//Bigint
+let number2= 864576845768457694576945769457684576845768457684576975467457689578n
+
+let name = "Sanyam Srivastava"
+
+let fhsdujh = true
+
+let id1 = Symbol("id")
+let id2 = Symbol("id")
+
+let hello; 
+let world = null
+
+
+console.log( number2, name, isActive, hello, world)
+console.log( typeof number2, typeof name, typeof isActive, typeof id1, typeof hello, typeof world)
