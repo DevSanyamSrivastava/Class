7ed355a840d1ed5ee83fb07e349bfab79767fd77
Sanyamm Srivastava <163429562+DevSanyamSrivastava@users.noreply.github.com>
2026-07-15 12:05:05 +0530

    Non Primitive data type

---
+++ 04_js/06_Non_Primitive.js
@@ -0,0 +1,18 @@
+//Object--In object we store data in key pair values
+
+let student ={
+    "id" :1,
+    "name" : "Sanyam",
+    "class" :10,
+    "Roll_No" : 34,
+    "address" : {
+        "City" : "lmp",
+        "Pincode" : 261506,
+        
+    }
+}
+
+console.log(student)
+console.log(student.Roll_No)
+console.log(student.address.Pincode)
+console.log(typeof student)
