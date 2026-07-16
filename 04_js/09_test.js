c0b36866fd7286364c87c797a3671d3a94250b65
DevSanyamSrivastava <sanyam.fullstackdev@gmail.com>
2026-07-16 12:23:35 +0530

    Assignment and Comparison Oprator

---
+++ 04_js/09_test.js
@@ -0,0 +1,34 @@
+//SImple intrest calculate :Pa, Rate, time
+
+let pa=600
+let rate=2
+let time =10
+
+// Approach1
+//  const total = (pa*rate*time)/100
+
+//  console.log(total)
+
+console.log((pa*rate*time)/100)
+
+
+
+
+
+let s1=80
+let s2 =70
+let s3 =90
+
+// const totalSum =(s1+s2+s3)*100
+// const percentage = totalSum/300
+
+// console.log(percentage)
+
+
+
+console.log(((s1+s2+s3)*100)/300)
+
+
+
+
+
---
+++ 04_js/10_comparison_oprator.js
@@ -0,0 +1,25 @@
+// let num1 = 30
+//  let num2 ="30"
+
+//  console.log(num1 == num2)
+//  console.log(num1 === num2)
+
+//  let num1 = 30
+//  let num2 ="30"
+//  console.log(num1 != num2)
+//  console.log(num1 !== num2)
+
+//  let num1 = 30
+//  let num2 =80
+//  console.log(num1 > num2)
+//  console.log(num1 < num2)
+
+//  let num1 = 70
+//  let num2 =70
+//  console.log(num1 >= num2)
+//  console.log(num1 <= num2)
+
+//  let num1 = 70
+//  let num2 =70
+//  console.log(num1 >= num2)
+//  console.log(num1 <= num2)
