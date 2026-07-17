c3e35061bc2fbd6a5dd7ca0de00da91ef69cf703
devayush7068 <dyayush25@gmail.com>
2026-07-17 12:19:12 +0530

    Update if else

--- 04_js/12_if-else.js
+++ 04_js/12_if-else.js
@@ -1,7 +1,46 @@
-let a = 6576876897987
-if(a>18 && a<150){
-    console.log("You are eligibile for vote")
-}
-else{
-     console.log("You are not eligibile for vote")
-}
+// let a = 6576876897987
+// if(a>18 && a<150){
+//     console.log("You are eligibile for vote")
+// }
+// else{
+//      console.log("You are not eligibile for vote")
+//}
+
+
+//user-defined values
+// let age = prompt("Enter your age")
+
+// if(age>18 && age<150){
+//     alert("You are eligibile for vote")
+// }
+// else{
+//      alert("You are not eligibile for vote")
+// }
+
+
+// let name = prompt("Enter you name")
+
+// alert(name)
+
+// let age =prompt("Enter your age")
+
+// alert(age)
+
+// let a=Number(prompt("Enter first No"))
+// let b=Number(prompt("Enter second No"))
+
+// const c =a+b
+
+// alert(c)
+
+
+let pa=Number(prompt("Enter Priciple Amount"))
+let rate=Number(prompt("Enter INtrest Rate "))
+let time =Number(prompt("Enter Time "))
+
+// Approach1
+//  const total = (pa*rate*time)/100
+
+//  console.log(total)
+
+alert((pa*rate*time)/100)
