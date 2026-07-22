dab40afade51d2b7679ef9facf78541bcb9fb2d8
DevSanyamSrivastava <sanyam.fullstackdev@gmail.com>
2026-07-22 12:26:52 +0530

    else if task

--- 04_js/13_elseif.js
+++ 04_js/13_elseif.js
@@ -8,19 +8,44 @@
 // prompt alert confirm 
 
 // let marks=prompt("Enter your marks")
-let marks= 90000
-if(marks>100 || marks<0){
-    alert("enter valid number")
+// let marks= 90000
+// if(marks>100 || marks<0){
+//     alert("enter valid number")
+// }
+// else if(marks>=90 && marks<100){
+//     alert("A+")  //ye block tb execute hoga jb  dono number condition follow karega
+// }
+// else if(marks>=80 && marks<90){
+//     alert("A")
+// }
+// else if(marks>70 && marks<80){
+//     alert("B")
+// }
+// else{
+//     alert("fail")
+// }
+
+
+// 0 , 100 --->Enter valid age
+// 1 to 18 --->You are underage
+// 19 to 60 --> you are eligible
+// 60 too 100 -->you are overage
+
+let age = 80
+if(age<=0 || age>100){
+    console.log("Enter valid age")
+}else if(age>=1 && age<=18){
+    console.log("You are underage")
+}else if(age>=19 && age<=60){
+    console.log("You are eligible")
+}else{
+    console.log("you are overage")
 }
-else if(marks>=90 && marks<100){
-    alert("A+")  //ye block tb execute hoga jb  dono number condition follow karega
-}
-else if(marks>=80 && marks<90){
-    alert("A")
-}
-else if(marks>70 && marks<80){
-    alert("B")
-}
-else{
-    alert("fail")
-}
+
+
+// If income is less than ₹20,000, print "Eligible for Government Support".
+// If income is between ₹20,000 and ₹49,999, print "Middle Income".
+// If income is between ₹50,000 and ₹99,999, print "High Income".
+// If income is between ₹1,00,000 and ₹1,99,999, print "Upper High Income".
+// If income is between ₹2,00,000 and ₹4,99,999, print "Rich".
+// If income is ₹5,00,000 or more, print "Luxury Income".
