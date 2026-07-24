db481438489f7900a458a7927e0471ed219e5419
DevSanyamSrivastava <sanyam.fullstackdev@gmail.com>
2026-07-24 12:22:43 +0530

    update switch case

---
+++ 04_js/15_switch_case.js
@@ -0,0 +1,29 @@
+// 90+ A+
+// 80 A
+// 70 B+
+// 60 B
+// 50 C
+// 40 D
+// 33 F
+let marks = '190'
+
+switch (marks){
+    case '90':
+        console.log("A+")
+    break;  
+    case '80':
+        console.log("A")
+    break;  
+    case '70':
+        console.log("B+")
+    break;  
+    case '60':
+        console.log("B")
+    break;  
+    case '50':
+        console.log("C")
+    break;  
+    default:
+        console.log("Not defined")
+
+}
---
+++ 04_js/16_for_loop.js
@@ -0,0 +1,15 @@
+//  for(i=0; i<=5; i++){
+//     console.log(i)
+//  }
+// table of number
+// for(i=1; i<=10; i++){
+//     console.log(3*i)
+// }
+
+// for(i=1; i<=49; i++){
+//     console.log(2*i+1)
+// }
+
+
+
+console.log(`I am Sanyam He says "We done good work" 'We all good'`)
