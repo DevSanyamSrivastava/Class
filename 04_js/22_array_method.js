0e32a4797d0dde711e36a67b7ff55e9c03364dfb
arunyadav5458-commits <arunyadav5458@gmail.com>
2026-08-02 12:23:54 +0530

    Clarify comments for array pop and push methods
    Updated comments to clarify the purpose of pop and push methods.

--- 04_js/22_array_method.js
+++ 04_js/22_array_method.js
@@ -1,8 +1,8 @@
 const num= [2,3,6,7,]
 
 // console.log(num.length)
-// num.pop()
-// num.push(7)
+// num.pop() it means remove the last number or words
+// num.push(7) it means add the number last 
 // console.log(num.length)
 // console.log(num)
 
