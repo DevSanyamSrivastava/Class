d15c91fd1307b3336d20b3ca3c2e4cc6eec79126
DevSanyamSrivastava <sanyam.fullstackdev@gmail.com>
2026-08-06 12:42:46 +0530

    react basics

--- 05_React/01_react/src/App.jsx
+++ 05_React/01_react/src/App.jsx
@@ -1,16 +1,13 @@
 
-
+import React from "react"
 
 function App() {
   
   return (
     <>
-  <h1>
-    Hello world
-  </h1>
-  <h2>
-    hello
-  </h2>
+      <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
   </>
   )
 }
---
+++ 05_React/02_react/src/components/Navbar.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+function Navbar() {
+return (
+  <nav
+    style={{
+      color: "red",
+      borderRadius: "10px",
+      borderWidth: "3px",
+      borderColor: "black",
+      borderStyle: "solid",
+      height: "40px",
+      width: "60px",        
+    
+    }}
+  >
+    Navbar
+  </nav>
+);
+
+}
+
+export default Navbar
