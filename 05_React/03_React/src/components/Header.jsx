18ed93e6fa0f748559e5441d7cf5604b02afcc98
DevSanyamSrivastava <sanyam.fullstackdev@gmail.com>
2026-08-07 18:34:06 +0530

    react

---
+++ 05_React/03_React/src/components/Header.jsx
@@ -0,0 +1,17 @@
+
+
+const Header=()=>{
+   return(
+   <nav className="navbar">
+   <ul className="nav-list">
+      <li>Home</li>
+      <li>About</li>
+      <li>Contact</li>
+      <li>Login</li>
+   
+   </ul>
+   </nav>
+   )
+}
+
+export default Header;
