0db43487aeb95e27b5446839caf51f6f6536e5ca
DevSanyamSrivastava <sanyam.fullstackdev@gmail.com>
2026-08-08 13:15:58 +0530

    tailwind setup

---
+++ 05_React/tailwindR/src/App.jsx
@@ -0,0 +1,21 @@
+
+
+
+function App() {
+
+
+  return (
+    <>
+   <h1 className="text-3xl font-bold underline bg-green-900">
+    Hello world!
+  </h1>
+  
+    </>
+  )
+}
+
+export default App
+
+
+
+
---
+++ 05_React/test/src/components/Header.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+import './Header.css'
+
+
+const Header=()=>{
+   return(
+   <nav className="navbar">
+   <ul className="nav-list">
+      <li>Home</li>
+      <li>About</li>
+      <li>Contact</li>
+      <li>Login</li>
+   
+   </ul>
+   </nav>
+   )
+}
+
+export default Header;
