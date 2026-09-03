5ab6a8daed0f62ffee370f3afce7152762ef6a73
DevSanyamSrivastava <sanyam.fullstackdev@gmail.com>
2026-09-03 18:36:21 +0530

    add category filter

--- 05_React/04_object/src/components/Api.jsx
+++ 05_React/04_object/src/components/Api.jsx
@@ -2,13 +2,20 @@ import React, { useEffect, useState } from "react";
 import "./api.css";
 
 const Api = () => {
+  const [category, setCategory] = useState("tech");
   const [articles, setArticles] = useState([]);
+  // const opinionFn=()=>{
+  //   setCategory("opinion")
+  // }
 
+  // const sportFn=()=>{
+  //   setCategory("sports")
+  // }
   useEffect(() => {
     const getArticles = async () => {
       try {
         const response = await fetch(
-          "https://noozra.com/api/articles?category=sports&limit=5"
+          `https://noozra.com/api/articles?category=${category}&limit=5`,
         );
 
         const data = await response.json();
@@ -19,11 +26,44 @@ const Api = () => {
     };
 
     getArticles();
-  }, []);
+  }, [category]);
 
   return (
     <div className="articles-container">
-      <h1>Sports Articles</h1>
+      <h1> {category} Articles</h1>
+      {/* <button className="btn" onClick={opinionFn}>Opinion</button> */}
+      <button
+        className="btn"
+        onClick={() => {
+          setCategory("opinion");
+        }}
+      >
+        Opinion
+      </button>
+      <button
+        className="btn"
+        onClick={() => {
+          setCategory("sports");
+        }}
+      >
+        Sports
+      </button>
+      <button
+        className="btn"
+        onClick={() => {
+          setCategory("world");
+        }}
+      >
+        World
+      </button>
+      <button
+        className="btn"
+        onClick={() => {
+          setCategory("entertainment");
+        }}
+      >
+        Entertainment
+      </button>
 
       {articles.map((allArticle) => (
         <div className="article-card">
@@ -47,3 +87,9 @@ const Api = () => {
 };
 
 export default Api;
+
+// opinion
+// world
+// politics
+// culture
+// entertainment
